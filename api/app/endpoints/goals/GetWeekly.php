f0c416ba8fd3bb6489fac5b3f315b4a68661e95a
Lawaty <abelrahmanlawaty@gmail.com>
2023-03-25 10:23:05 +0200

    up

--- api/app/endpoints/goals/GetWeekly.php
+++ api/app/endpoints/goals/GetWeekly.php
@@ -16,7 +16,7 @@ class GetWeekly extends Authenticated
 
     $dates = (new Ndate($this->request['date']))->getWeekDates();
 
-    $week_goals = GoalMapper::getAllByUser(
+    $temp = GoalMapper::getAllByUser(
       $this->user,
       [
         ' AND (',
@@ -25,7 +25,26 @@ class GetWeekly extends Authenticated
       ]
     );
 
+    $week_goals = [];
+    foreach($temp as $goal) {
+      if (
+        !isset($week_goals[$goal->get('date')]) ||
+        $goal->get('day') == 'NIL'
+      )
+      $week_goals[$goal->get('date')] = $goal;
+    }
+    $week_goals = array_values($week_goals);
+
+    foreach ($week_goals as $goal) {
+      if (
+        !isset($formatted[$goal->get('project_id')]) ||
+        $goal->get('day') == 'NIL'
+      )
+        $formatted[$goal->get('project_id')] = $goal;
+    }
+
     $formatted = [];
+
     foreach ($week_goals as $goal) {
       $project_id = $goal->get('project_id');
       if (!isset($formatted[$project_id]))
@@ -42,11 +61,11 @@ class GetWeekly extends Authenticated
       ]
     );
 
-    foreach($week_sessions as $session)
+    foreach ($week_sessions as $session)
       $formatted[$session->get('project_id')]->calcSession($session);
 
     foreach ($formatted as &$goal)
-    $goal = $goal->get('amount', 'progress');
+      $goal = $goal->get('amount', 'progress');
 
     return new Response(SUCCESS, $formatted);
   }
